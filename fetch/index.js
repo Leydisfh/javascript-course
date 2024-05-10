a357acc26fa225418e2b3753f660c059d5324736
Leydis <leydisfh@gmail.com>
2024-05-10 09:56:08 -0500

    LLamando a una api

--- fetch/index.js
+++ fetch/index.js
@@ -1,1 +1,4 @@
+// esta es la URL de la API que vamos a consumir
+const baseUrl = 'https://rickandmortyapi.com/api/character';
 
+fetch(`${baseUrl}`) // fetch is a function that returns a promise
--- fetch/readme.md
+++ fetch/readme.md
@@ -1,3 +1,2 @@
- Fetch se utiliza para obtener el resultado de una determinada URL sin recargar la página.
 
  
