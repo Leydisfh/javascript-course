5f9c9d909cc71e6b8c41ec10511cf5dd2ec2a775
Leydis <leydisfh@gmail.com>
2024-05-10 15:03:29 -0500

    Add fetch functionality for retrieving data from a URL

--- fetch/index.js
+++ fetch/index.js
@@ -1,4 +1,18 @@
-// esta es la URL de la API que vamos a consumir
-const baseUrl = 'https://rickandmortyapi.com/api/character';
+// >> Este es un ejemplo de como consumir una API con fetch, en este caso la API de Rick and Morty
+// >> Hacemos la petición a la API y obtenemos la información de un personaje en específico 
 
-fetch(`${baseUrl}`) // fetch is a function that returns a promise
+
+// esta es la URL de la API que vamos a consumir
+const baseUrl = 'https://rickandmortyapi.com/api/character/2';
+
+fetch(baseUrl) // fetch is a function that returns a promise
+.then(response => response.json()) // response.json() returns a promise
+.then(data => { // data is the resolved value of the promise returned by response.json()
+    console.log(data) // Nos permite observar la data que nos devuelve la API
+    info(data);
+});
+
+
+const info = (data) =>{
+    console.log(`The name of the character is ${data.name} and the status is ${data.status}`)
+}
