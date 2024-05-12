b80f772a18c81feacfae31a54dbddbd4fad7a9cf
Leydis <leydisfh@gmail.com>
2024-05-12 12:41:52 -0500

    Add fetch functionality for retrieving data from a URL

--- fetch/index.js
+++ fetch/index.js
@@ -11,8 +11,50 @@ fetch(baseUrl) // fetch is a function that returns a promise
     console.log(data) // Nos permite observar la data que nos devuelve la API
     info(data);
 });
-
-
 const info = (data) =>{
     console.log(`The name of the character is ${data.name} and the status is ${data.status}`)
 }
+
+
+//-----------------------------------------------------------------------------------------------------------
+// Creando una clase constructora para consumir una Api
+
+class FetchWrapper {
+    constructor(baseUrl){ // constructor es un método especial de una clase para crear e inicializar un objeto creado con una clase
+        this.baseUrl = baseUrl;
+    }
+    get(endpoint){ 
+        return fetch(this.baseUrl + endpoint) 
+        .then(response => response.json())
+    }
+}
+// Instanciamos la clase FetchWrapper
+const API = new FetchWrapper('https://rickandmortyapi.com/api/character/'); 
+// Hacemos la petición a la API y obtenemos la información de un personaje en específico
+API.get('2')
+.then(data => console.log(data)) // Nos permite observar la data que nos devuelve la API
+
+//-----------------------------------------------------------------------------------------------------------
+// Creamos una clase para implementar un PUT a la API
+class FetchWrapper{
+    constructor(baseUrl){
+        this.baseUrl = baseUrl;
+    }
+    PUT(endpoint, body){
+        return fetch(this.baseUrl + endpoint, {
+            method: 'PUT',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(body) 
+        })
+        .then(response => response.json())
+    }
+}
+const Api  = new FetchWrapper("https://rickandmortyapi.com/api/character/")
+Api.PUT('2', {name: 'Morty', status: 'Alive'})
+.then(data => console.log(data))
+
+// -----------------------------------------------------------------------------------------------------------
+// Creamos una clase para implementar un POST y Deleted a la API
+
